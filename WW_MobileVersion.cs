c5007bae96c8a5873ccf47b59ea668d21211d6f9
White Wolf Game Studio <58172779+WhiteWolfStudio@users.noreply.github.com>
2021-09-08 23:43:56 +0200

    Add files via upload

--- WW_MobileVersion.cs
+++ WW_MobileVersion.cs
@@ -3,45 +3,49 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
-public class WW_MobileVersion : MonoBehaviour {
+namespace WhiteWolf {
 
-    [Space]
+    public class WW_MobileVersion : MonoBehaviour {
 
-    public bool _loadScene;
-    public string _pc;
-    public string _mobile;
+        [Space]
 
-    [Space]
-    [Space]
-    [Space]
+        public bool _loadScene;
+        public string _pc;
+        public string _mobile;
 
-    public bool ifMobileActive;
+        [Space]
+        [Space]
+        [Space]
+
+        public bool ifMobileActive;
     
-    [Header("Active - True")]
-    public GameObject[] objsTrue;
+        [Header("Active - True")]
+        public GameObject[] objsTrue;
 
-    [Header("Active - False")]
-    public GameObject[] objsFalse;
+        [Header("Active - False")]
+        public GameObject[] objsFalse;
 
-    /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
-    string platform;
+        string platform;
 
-    /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
-    void Awake(){
+        void Awake(){
 
-        platform = Application.platform.ToString();
+            platform = Application.platform.ToString();
 
-        if ( _loadScene ){
+            if ( _loadScene ){
 
-            if ( platform != RuntimePlatform.IPhonePlayer.ToString() && platform != RuntimePlatform.Android.ToString() ){ SceneManager.LoadScene( _pc ); }
-            else { SceneManager.LoadScene( _mobile ); }
+                if ( platform != RuntimePlatform.IPhonePlayer.ToString() && platform != RuntimePlatform.Android.ToString() ){ SceneManager.LoadScene( _pc ); }
+                else { SceneManager.LoadScene( _mobile ); }
+
+            }
+
+            if ( ifMobileActive ){ for ( int i=0; i<objsTrue.Length; i++ ){ objsTrue[ i ].SetActive( true ); } }
+            if ( ifMobileActive ){ for ( int i=0; i<objsFalse.Length; i++ ){ objsFalse[ i ].SetActive( false ); } }
 
         }
-
-        if ( ifMobileActive ){ for ( int i=0; i<objsTrue.Length; i++ ){ objsTrue[ i ].SetActive( true ); } }
-        if ( ifMobileActive ){ for ( int i=0; i<objsFalse.Length; i++ ){ objsFalse[ i ].SetActive( false ); } }
 
     }
 
